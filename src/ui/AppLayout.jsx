d07929e5b7348d28ab3601a97c3a073b064c1d18
Gasirbek.T <gasirbek_ernazaruli3112@mail.ru>
2023-09-25 12:33:45 +0500

    updated styles

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,12 +8,14 @@ function AppLayout() {
   const isLoding = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoding && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl ">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
